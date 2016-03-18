15afd437aeb1a550bda0e6f1030a2cac50b7f72b
capstat <ncapofari@yahoo.com>
2016-03-18 09:55:14 -0400

    Update server.R

--- HW_3_1/server.R
+++ HW_3_1/server.R
@@ -3,7 +3,7 @@ suppressPackageStartupMessages(library(dplyr))
 library(shiny)
 
 #get the csv file from github
-state_df = read.csv("https://raw.githubusercontent.com/jlaurito/CUNY_IS608/master/lecture3/data/cleaned-cdc-mortality-1999-2010.csv",
+state_df = read.csv("cleaned-cdc-mortality-1999-2010.csv",
                     stringsAsFactors=FALSE)
 #keep only 2010 data
 state_df = state_df[state_df$Year == 2010,]
